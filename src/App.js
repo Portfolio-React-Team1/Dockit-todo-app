bbe01ea400d6fe136e9594cc539cce4077f6750f
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-06-16 00:08:17 +0900

    Landing page updated
    Created Carousel Component for onboarding screens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Landing from "./pages/Landing_page/Landing";
+import Carousel, { CarouselItem } from "./pages/Landing_page/Carousel";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Carousel>
+        <CarouselItem>
+          <Landing />
+        </CarouselItem>
+        <CarouselItem>Manage your tasks component Page</CarouselItem>
+        <CarouselItem>create daily routine component</CarouselItem>
+        <CarouselItem>organize your task component</CarouselItem>
+      </Carousel>
     </div>
   );
 }
