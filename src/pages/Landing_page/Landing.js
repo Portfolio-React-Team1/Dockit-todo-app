1ac50fa086d96ce5a09e6b9efdab51a722dc782a
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-06-16 20:42:52 +0900

    Landing page updated
    Created onboarding 1screen component

--- src/pages/Landing_page/Landing.js
+++ src/pages/Landing_page/Landing.js
@@ -1,10 +1,23 @@
 import "./Landing.css";
 import Intro from "./Intro.js";
+import Carousel, { CarouselItem } from "./Carousel";
+import TaskManager from "./TaskManager";
 
 function Landing() {
   return (
-    <div className="container">
-      <Intro />
+    <div>
+      <Carousel>
+        <CarouselItem>
+          <Intro />
+        </CarouselItem>
+        <CarouselItem>
+          {" "}
+          <TaskManager />{" "}
+        </CarouselItem>
+        <CarouselItem>create daily routine component</CarouselItem>
+        <CarouselItem>organize your task component</CarouselItem>
+      </Carousel>
+      <TaskManager />
     </div>
   );
 }
---
+++ src/pages/Landing_page/TaskManager.js
@@ -0,0 +1,31 @@
+import "./Onboarding.css";
+import Group182 from "../../assets/images/group182.png";
+
+function TaskManager() {
+  return (
+    <div className="container-center-horizontal">
+      <div className="onboarding-1 screen">
+        <div className="skip">
+          <span> Skip</span>
+        </div>
+        <img className="group-182" src={Group182} alt="busy man" />
+        <div className="navi">
+          <div className="rectangle-100"> </div>
+          <div className="rectangle-10"> </div>
+          <div className="rectangle-10"> </div>
+        </div>
+        <h1 className="screen-title">
+          <span> Manage your tasks</span>
+        </h1>
+        <p className="easy-mgt">
+          {" "}
+          <span>
+            {" "}
+            You can easily manage all your daily tasks on Dockit for free
+          </span>{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
+export default TaskManager;
