d5c4e7d6a7e2d36b65affe7a7736252da3ade244
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-06-17 22:21:25 +0900

    Landing page updated
    Start screen component created.

--- src/pages/Landing_page/Landing.js
+++ src/pages/Landing_page/Landing.js
@@ -3,6 +3,8 @@ import Intro from "./Intro.js";
 import Carousel, { CarouselItem } from "./Carousel";
 import Onboarding1 from "./Onboarding1";
 import Onboarding2 from "./Onboarding2";
+import Onboarding3 from "./Onboarding3";
+import StartScreen from "./StartScreen";
 
 function Landing() {
   return (
@@ -18,11 +20,17 @@ function Landing() {
         <CarouselItem>
           <Onboarding2 />
         </CarouselItem>
-        <CarouselItem>organize your task component</CarouselItem>
+        <CarouselItem>
+          <Onboarding3 />
+        </CarouselItem>
+        <CarouselItem>
+          <StartScreen />
+        </CarouselItem>
       </Carousel>
       <Onboarding1 />
-      <hr />
       <Onboarding2 />
+      <Onboarding3 />
+      <StartScreen />
     </div>
   );
 }
