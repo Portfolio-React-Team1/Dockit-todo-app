87b49aa1ef280e4816fc41ae0298cf4061d5f479
Abubakarsadiqq <abba24452@gmail.com>
2022-06-18 08:02:54 +0100

    updated the login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,7 +3,7 @@ import './Login.css';
 import {FaChevronLeft,FaGoogle,FaApple} from 'react-icons/fa';
 import {ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 
 const Login = () => {
