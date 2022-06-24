d7c132073784aeb4ca10a287f693ea0de1622c5d
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-06-25 02:40:13 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Landing from "./pages/Landing_page/Landing";
 
-//import App from "./App";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Landing />
+    <App />
   </React.StrictMode>
 );
